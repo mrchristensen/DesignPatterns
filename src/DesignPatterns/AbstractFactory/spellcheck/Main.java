e90782a33056614b896131f254a501e8e5694ec3
Matt <mr.christensen@gmail.com>
2020-03-09 17:50:56 -0600

    Cleaned up Abstract Factory code

--- src/DesignPatterns/AbstractFactory/spellcheck/Main.java
+++ src/DesignPatterns/AbstractFactory/spellcheck/Main.java
@@ -15,11 +15,10 @@ public class Main {
 	
 		try {
 			String content = args[0];
-			//pass in concrete factories
 			SpellingChecker checker = new SpellingChecker(new URLDocumentSourceFactory(), new PlainTextDocumentParserFactory(), new TextFileDictionaryFactory());
 			SortedMap<String, Integer> mistakes = checker.check(content);
 			System.out.println("Mistakes:\n" + mistakes);
-			System.out.println("Test: make sure output matches: \n" + "{be=1, doesn=1, funtion=1, t=2}"); //Matt said that we can do it this way
+			System.out.println("Test: make sure output matches: \n" + "{be=1, doesn=1, funtion=1, t=2}");
 		}
 		catch (IOException e) {
 			e.printStackTrace();
